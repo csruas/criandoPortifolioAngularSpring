c586d8b69d80f09c88bbaa087bbd2e6ffe9b4500
Cristiano de Sousa Ruas <cristiano.renuncia@gmail.com>
2025-04-26 23:54:34 -0300

    =Iniciando o projeto e componentes com a estruturando o projeto

---
+++ alugarCar/src/app/components/layout/login/login.component.ts
@@ -0,0 +1,30 @@
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { MdbFormsModule } from 'mdb-angular-ui-kit/forms';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [MdbFormsModule, FormsModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
+})
+export class LoginComponent {
+  usuario: string = '';
+  senha: string = '';
+
+  router = inject(Router);
+
+  constructor() { }
+  login() {
+    if (this.usuario === 'admin' && this.senha === 'admin') {
+      alert('Login realizado com sucesso!');
+      this.router.navigate(['admin/carros']);
+
+    } else {
+      alert('Usuario ou senha estão incorreto');
+    }
+  }
+
+}
